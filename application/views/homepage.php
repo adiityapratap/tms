6b0484a498c417f534c4b955317e6144982728f5
adityapratap <aditya@gorilladesk.com>
2021-01-21 22:00:57 +0530

    new chnages

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -21,6 +21,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	.heading_image{
 		background-image: url('images/title.png');
 	}
+  .task_listing {
+    background-color: white;
+  }
+
+  .close {
+    border-radius: 30px;
+    color: white;
+    cursor: pointer;
+    position: absolute;
+    /* top: 57%; */
+    right: 0%;
+    padding: 8px 12px;
+    transform: translate(0%, -50%);
+    font-size: 18px;
+    background-color: black;
+}
+
+.close:hover {background: #bbb;}
 	</style>
 </head>
 <body>
@@ -49,10 +67,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       <span class="text-danger"></span>
      </div>
      <div class="form-group">
+       <input type="hidden" id="task_id" name="task_id" value="" />
       <input type="button" id="save_task" value="Save" class="btn btn-info" />
+      <input type="button" onclick="viewtask()" value="View" class="btn btn-info" />
      </div>
     </form>
    </div>
+  </div>
+
+  <div id="task_list">
+    
   </div>
  </div>
 
@@ -61,8 +85,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script>
 $("#save_task").on('click',function(){
-  
-$.ajax({
+           
+          
+            if( localStorage["id"]==undefined ){
+             var task_id = 1;
+            } else{
+              var task_id = parseFloat(localStorage.getItem("id")) + parseFloat(1);
+            }
+            $("#task_id").val(task_id);
+            var post_vars = $('#task_form').serialize();
+            localStorage.setItem('id',task_id);
+            localStorage.setItem(task_id,post_vars);
+           
+
+            $.ajax({
                 url:'<?php echo base_url();?>index.php/homepage/save_task',
                 method:"POST",
                 data:$("#task_form").serialize(),
@@ -73,6 +109,73 @@ $.ajax({
               })
 
 
+    $("#task_list").html('');
+    $all_tasks = allStorage();
+    $.each( $all_tasks, function( key, value ) {
+      var input = deserialize(value);
+     $("#task_list").append('<div class="task_listing" id="'+input.task_id+'"><b>'+input.task_name+'</b><p>'+input.task_date+'</p><p>'+input.task_detail+'</p> <span class="close" onclick="delete_task('+input.task_id+')">x</span><div>');
+   
+     });
+ 
+
 });
+
+function viewtask(){
+  $("#task_list").html('');
+    $all_tasks = allStorage();
+    $.each( $all_tasks, function( key, value ) {
+      var input = deserialize(value);
+     $("#task_list").append('<div class="task_listing" id="'+input.task_id+'"><b>'+input.task_name+'</b><p>'+input.task_date+'</p><p>'+input.task_detail+'</p> <span class="close" onclick="delete_task('+input.task_id+')">x</span> <div>');
+   
+     });
+}
+
+$(function(){
+   viewtask();
+});
+
+function allStorage() {
+
+        var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+    while ( i-- ) {
+      if (localStorage.getItem(keys[i]).length > 2) {
+        values.push(localStorage.getItem(keys[i]) );
+      }
+        
+    }
+
+    return values;
+}
+
+function delete_task(task_id){
+  console.log(task_id);
+localStorage.removeItem(task_id);
+viewtask();
+}
+
+function deserialize(txt){
+    myjson={}
+    tabparams= txt.split('&')
+    var i=-1;
+    while (tabparams[++i]){
+    tabitems=tabparams[i].split('=')
+    if( myjson[decodeURIComponent(tabitems[0])] !== undefined ){
+        if( myjson[decodeURIComponent(tabitems[0])] instanceof Array ){
+            myjson[decodeURIComponent(tabitems[0])].push(decodeURIComponent(tabitems[1]))
+        }
+    else{
+       myjson[decodeURIComponent(tabitems[0])]= [myjson[decodeURIComponent(tabitems[0])],decodeURIComponent(tabitems[1])]
+            }
+        }
+    else{
+         myjson[decodeURIComponent(tabitems[0])]=decodeURIComponent(tabitems[1]);
+        }
+    }
+    return myjson;
+    }
+
 
 </script>
